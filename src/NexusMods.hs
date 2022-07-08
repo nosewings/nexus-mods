0ff70eead92348cec9dfddb687aa44ecf6b087f2
Nicholas Coltharp <coltharp@pdx.edu>
2022-07-08 15:32:15 -0500

    Add `trackMod` and `untrackMod`
    - Add the `Message` type to capture the output of API calls that return
    messages.
    - Add `sop-core` dependency so that we can match on the results of
    `UVerb` calls.
    - Add `GADTs` language feature, also for matching on `UVerb` calls.

--- src/NexusMods.hs
+++ src/NexusMods.hs
@@ -2,12 +2,16 @@ module NexusMods (
   User (..),
   validate,
   getTrackedMods,
+  trackMod,
+  untrackMod,
   runNexus,
 ) where
 
 import Data.Aeson
 import Data.Aeson.TH
 import Data.Data
+import Data.Functor
+import Data.SOP.NS
 import Network.HTTP.Client hiding (Proxy)
 import Network.HTTP.Client.TLS
 import Servant.API
@@ -34,9 +38,28 @@ data ModRef = ModRef
 
 deriveFromJSON (defaultOptions {fieldLabelModifier = camelTo2 '_'}) ''ModRef
 
+newtype Message = Message
+  { message :: String
+  }
+  deriving (Eq, Ord, Read, Show)
+
+deriveFromJSON (defaultOptions {fieldLabelModifier = camelTo2 '_'}) ''Message
+
 type NexusModsAPI =
   "v1" :> "users" :> "validate.json" :> Header' '[Required] "apikey" String :> Get '[JSON] User
     :<|> "v1" :> "user" :> "tracked_mods.json" :> Header' '[Required] "apikey" String :> Get '[JSON] [ModRef]
+    :<|> ( "v1" :> "user" :> "tracked_mods.json"
+            :> Header' '[Required] "apikey" String
+            :> QueryParam' '[Required] "domain_name" String
+            :> QueryParam' '[Required] "mod_id" Int
+            :> UVerb POST '[JSON] '[WithStatus 200 Message, WithStatus 201 Message]
+         )
+    :<|> ( "v1" :> "user" :> "tracked_mods.json"
+            :> Header' '[Required] "apikey" String
+            :> QueryParam' '[Required] "domain_name" String
+            :> QueryParam' '[Required] "mod_id" Int
+            :> UVerb DELETE '[JSON] [WithStatus 200 Message, WithStatus 404 Message]
+         )
 
 api :: Proxy NexusModsAPI
 api = Proxy
@@ -46,7 +69,31 @@ validate :: String -> ClientM User
 
 -- | Get a user's list of tracked mods.
 getTrackedMods :: String -> ClientM [ModRef]
-validate :<|> getTrackedMods = client api
+
+-- | Internal version of @trackMod@.
+trackMod' :: String -> String -> Int -> ClientM (Union '[WithStatus 200 Message, WithStatus 201 Message])
+
+-- | Start tracking a mod.  Returns @True@ if the user was not already
+-- tracking the mod.
+trackMod :: String -> String -> Int -> ClientM Bool
+trackMod k d m =
+  trackMod' k d m <&> \case
+    Z _ -> False
+    _ -> True
+
+-- | Internal version of @untrackMod@.
+untrackMod' :: [Char] -> [Char] -> Int -> ClientM (Union '[WithStatus 200 Message, WithStatus 404 Message])
+
+-- | Stop tracking a mod.  Returns @True@ if the user was previously
+-- tracking the mod.
+untrackMod :: String -> String -> Int -> ClientM Bool
+untrackMod a b c =
+  untrackMod' a b c <&> \case
+    Z _ -> True
+    _ -> False
+
+-- | Create the API functions.
+validate :<|> getTrackedMods :<|> trackMod' :<|> untrackMod' = client api
 
 -- | Run a Nexus Mods API computation.  This is a convenience function
 -- that uses HTTPS and the default Nexus Mods URL.
