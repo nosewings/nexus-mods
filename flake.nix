0d0cf6405c7b16b8f54339482e4d8238149e61d5
Nicholas Coltharp <coltharp@pdx.edu>
2022-08-16 04:58:33 -0500

    Improve flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "Flake for nexus-mods";
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
