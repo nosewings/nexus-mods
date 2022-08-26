e85af17c67d1748a29e780bb4875933d1418bd3e
Nicholas Coltharp <coltharp@pdx.edu>
2022-08-25 19:15:27 -0500

    Add `FromHttpApiData` for `MD5String`

--- src/NexusMods/MD5String.hs
+++ src/NexusMods/MD5String.hs
@@ -1,9 +1,12 @@
 module NexusMods.MD5String (
   MD5String,
   fromString,
+  fromText,
   toString,
 ) where
 
+import Data.Text (Text)
+import Data.Text qualified as Text
 import Control.Applicative
 import Control.Category ((>>>))
 import Data.Char
@@ -25,6 +28,10 @@ instance Read MD5String where
 instance ToHttpApiData MD5String where
   toUrlPiece = toUrlPiece . toString
 
+instance FromHttpApiData MD5String where
+  -- NOTE error message taken from the NexusMods server
+  parseUrlPiece = maybe (Left "That is not a valid MD5 Hash") Right . fromText
+
 lengthIs :: Int -> [a] -> Bool
 lengthIs 0 [] = True
 lengthIs n (_ : xs) | n > 0 = lengthIs (n - 1) xs
@@ -33,6 +40,13 @@ lengthIs _ _ = False
 fromString :: String -> Maybe MD5String
 fromString s
   | lengthIs 32 s && all isHexDigit s = Just (MD5String s)
+  | otherwise = Nothing
+
+-- Different from `fromString` because `Text.length s == 32` is much
+-- faster than `lengthIs 32 s`.
+fromText :: Text -> Maybe MD5String
+fromText s
+  | Text.length s == 32 && Text.all isHexDigit s = Just . MD5String . Text.unpack $ s
   | otherwise = Nothing
 
 toString :: MD5String -> String
